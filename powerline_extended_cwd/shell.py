17507b8b7b03405cdcaf16ebb34794ef82750abd
Pavel Pushkarev <riarheos@gmail.com>
2018-07-29 01:06:21 +0300

    Added hostname support in xterm

--- powerline_extended_cwd/shell.py
+++ powerline_extended_cwd/shell.py
@@ -5,6 +5,7 @@ from powerline.segments.common.env import CwdSegment
 from powerline.lib.unicode import out_u
 import os
 import yaml
+import socket
 
 
 @requires_segment_info
@@ -18,7 +19,11 @@ class ShellCwdSegment(CwdSegment):
             return '[not found]'
 
     @staticmethod
-    def xterm_header_segment(text):
+    def xterm_header_segment(segment_info, cwd):
+        if segment_info['environ'].get('SSH_CLIENT'):
+            text = socket.gethostname() + ': ' + cwd
+        else:
+            text = cwd
         return { 'literal_contents': (0, '\[\033]0;%s\007\033]6;1;bg;*;default\a\]' % text) }
 
     @staticmethod
@@ -28,16 +33,16 @@ class ShellCwdSegment(CwdSegment):
         else:
             return { 'contents': text }
 
-    def substitute(self, cwd, path, replacement, color):
+    def substitute(self, segment_info, cwd, path, replacement, color):
         cwd = cwd[len(path) + 1:]
         if cwd:
             return [
-                self.xterm_header_segment('{}/{}'.format(replacement, cwd)),
+                self.xterm_header_segment(segment_info, '{}/{}'.format(replacement, cwd)),
                 self.regular_segment(replacement, color),
                 self.regular_segment(cwd),
             ]
 
-        return [ self.xterm_header_segment(replacement), self.regular_segment(replacement, color) ]
+        return [ self.xterm_header_segment(segment_info, replacement), self.regular_segment(replacement, color) ]
 
     def __call__(self, pl, segment_info, shrink=[], **kwargs):
         cwd = self.get_cwd(segment_info)
@@ -45,18 +50,18 @@ class ShellCwdSegment(CwdSegment):
 
         # no home => nothing to shrink
         if not home:
-            return [self.xterm_header_segment(cwd), self.regular_segment(cwd)]
+            return [self.xterm_header_segment(segment_info, cwd), self.regular_segment(cwd)]
 
         # check if path is in the config
         for item in shrink:
             path = item[0].replace('~', home)
             if cwd.startswith(path):
-                return self.substitute(cwd, path, item[1], 'cwd:split')
+                return self.substitute(segment_info, cwd, path, item[1], 'cwd:split')
 
         # finally the non-configured home
         if cwd.startswith(home):
-            return self.substitute(cwd, home, '~', 'cwd:home')
+            return self.substitute(segment_info, cwd, home, '~', 'cwd:home')
 
-        return [self.xterm_header_segment(cwd), self.regular_segment(cwd)]
+        return [self.xterm_header_segment(segment_info, cwd), self.regular_segment(cwd)]
 
 cwd = ShellCwdSegment()
